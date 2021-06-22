ec280634821c1b86689eda6f15797d138d9f4d4a
Art-Ben <artem.benevskiy@gmail.com>
2021-06-22 12:11:43 +0300

    Become An Agent v1

--- template-parts/bana/section2.php
+++ template-parts/bana/section2.php
@@ -1,1 +1,25 @@
 <?php
+$title = get_field('sect2_title');
+$smalltitle = get_field('sect2_smalltitle');
+$content = get_field('sect2_content');
+?>
+
+<section class="pageBecomeAnAgent_sect2">
+    <div class="basic-container pageBecomeAnAgent_sect2__cont">
+        <div class="column">
+            <h2 class="cstTitle">
+                <?= $title; ?>
+            </h2>
+            <span class="pageBecomeAnAgent__smalltitle">
+                <div class="pageBecomeAnAgent__content">
+                    <?= $smalltitle; ?>
+                </div>
+            </span>
+        </div>
+        <div class="column">
+            <?= $content; ?>
+        </div>
+    </div>
+</section>
+
+
